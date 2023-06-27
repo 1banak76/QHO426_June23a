10f4280fd1a9fa8fcb2ca62331d68019991f6759
1banak76 <1banak76@solent.ac.uk>
2023-06-27 11:28:10 +0100

    Seminar week 1 completed

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+# Displays escape characters
+print("\n Displays new line")
+print("\t Displays a tab")
+print("\\ Displays a backslash")
+print("Displays a double quote")
+print("\' display a single quote")
