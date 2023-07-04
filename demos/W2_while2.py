7a5040b9ab9c39502f53b72f2c63aa6d00a89b7d
1banak76 <1banak76@solent.ac.uk>
2023-07-04 11:21:08 +0100

    Week 2 seminar done

---
+++ demos/W2_while2.py
@@ -0,0 +1,12 @@
+ans = ""
+
+while ans != "stop":
+    print("DorMamu, I came to bargain")
+    ans = input()
+print("Dr Strange won!")
+
+while True:
+    print("Hahaha")
+    inp = input()
+    if inp == "426":
+        break
